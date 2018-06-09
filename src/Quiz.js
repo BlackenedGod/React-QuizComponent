e5e5109e74ab33c0d239c82befe8fe52ae042205
Kerem Kambur <kerem.kambur@hotmail.com>
2018-06-10 00:25:39 +0300

    First step

---
+++ src/Quiz.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+import quizData from './quiz_data.json';
+
+export default class Quiz extends Component {
+    
+    constructor(props){
+        super(props);
+
+        this.state = {
+            quiz_position: 1
+        };
+    }
+    
+    render(){
+        return(
+            <div>
+                <div className="QuizQuestion">
+                    {quizData.quiz_questions[0].instruction_text}
+                </div>
+            </div>
+        );
+    }
+}
