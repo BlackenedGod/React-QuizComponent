32d6e7b070ccbd74e02501ec0d7a45a5518bc8fa
Kerem Kambur <kerem.kambur@hotmail.com>
2018-06-10 04:16:13 +0300

    mod 7

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,11 +3,24 @@ import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component{
     
-    
+    constructor(props){
+        super(props);
+        this.state = {
+            incorrectAnswer : false
+        }
+    }
 
     handleClick(buttonText){
         if(buttonText === this.props.quiz_question.answer){
             this.props.showNextQuestionHandler();
+            this.setState((state) => {
+                return {incorrectAnswer: false}
+            });
+        }
+        else{
+            this.setState((state) => {
+                return {incorrectAnswer: true}
+            });
         }
     }
 
@@ -26,6 +39,7 @@ class QuizQuestion extends Component{
                         )}
                 </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className="error">That's not right.</p> : null}
             </main>
         );
     }
